22dc03c8bd9f5f78b16f8afc45e13007f98012c4
Sam Russell <sam.p.russell@gmail.com>
2019-07-23 22:53:24 -0230

    eslint stuff

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const fs = require("fs");
-const express = require('express')
-const app = express()
-const port = 3000
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/recipes', function getRecipes(req, res) {
-    let contents = fs.readFileSync("./recipes.json")
-    res.send(JSON.parse(contents));
+  const contents = fs.readFileSync('./recipes.json');
+  res.send(JSON.parse(contents));
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $.get({
-    url: "/recipes",
-    success: function( result ) {
-        let s = JSON.stringify(result);
-        $('#json').append(s);
-    }
-  });
+  url: '/recipes',
+  success: function( result ) {
+    const s = JSON.stringify(result);
+    $('#json').append(s);
+  },
+});
