380bfaf23b9da6a2216b8d783b69100812b4bcd2
Sam Russell <sam.p.russell@gmail.com>
2020-03-05 23:53:47 -0330

    updates. pulled in data from recipe API for pies. updated to display items based on json from server. vanilla js at the moment.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const port = 3000;
 
@@ -8,6 +9,16 @@ app.use(express.static('public'));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 app.get('/recipes', function getRecipes(req, res) {
-  const contents = fs.readFileSync('./recipes.json');
+  const contents = fs.readFileSync('./pies.json');
   res.send(JSON.parse(contents));
+});
+
+app.get('/load-recipes', (req, res) => {
+  fetch('https://api.edamam.com/search?app_id=900da95e&app_key=40698503668e0bb3897581f4766d77f9&q=pie')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        fs.writeFileSync('./pies.json', JSON.stringify(data.hits));
+      });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,32 @@
-$.get({
-  url: '/recipes',
-  success: function( result ) {
-    const s = JSON.stringify(result);
-    $('#json').append(s);
-  },
-});
+/* eslint-disable linebreak-style */
+fetch("/recipes")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    loadRecipeCards(data);
+  });
+
+function loadRecipeCards(recipeJson) {
+  let container = document.querySelector(".container");
+  recipeJson.forEach(entry => {
+    let recipe = entry.recipe;
+    let ingredients = "";
+    recipe.ingredientLines.forEach(ingredient => {
+      ingredients += `<li>${ingredient}</li>`;
+    });
+    let div = document.createElement("div");
+    div.className = "card";
+    div.innerHTML = `      
+      <div class="hero"><img src="${recipe.image}" alt=""></div>
+      <div class="recipe">
+        <h1>${recipe.label}</h1>
+        <h3>Ingredients</h3>
+        <ul>
+          ${ingredients}
+        </ul>
+      </div>
+    `;
+    container.appendChild(div);
+  });
+}
